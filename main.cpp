4894b8749f1999d8c84e7e0e01e1b092ae52752c
Piotr Szymajda <PiotrSzymajda@10g.pl>
2016-03-09 15:42:10 +0100

    Remove comment

--- main.cpp
+++ main.cpp
@@ -2,8 +2,6 @@
 Autor: Piotr Szymajda - 273 023
 
 Program typu "Traceroute" - pierwsze zad. programistyczne na SK
-Wersja nieukończona, wysłana w celu zabezpieczenia przed czynnikami losowymi,
-mogącymi wpłynać na nie oddanie zadania w terminie.
 */
 #include <iostream>
 #include <cstring>
